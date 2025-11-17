89abcd3a3d7f6c85fce2208f1101611e6d78c1e1
Ahmed Samir <ahmedsemido14@gmail.com>
2025-11-17 22:22:31 +0200

    Add Product Module core entities: Product, ProductBrand, ProductType

---
+++ DomainLayer/ECommerce.Domain/Models/Products/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Models.Products
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string PictureURL { get; set; } = null!;
+        public decimal Price { get; set; }
+
+
+        public int productBrandId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = null!;
+
+
+        public int ProductTypeId { get; set; }
+        public ProductType ProductType { get; set; } = null!;
+    }
+}
