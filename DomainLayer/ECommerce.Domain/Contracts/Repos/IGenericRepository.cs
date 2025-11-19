7a06d5ffe1ec5120f7596f8268272c4baf0218c9
Ahmed Samir <ahmedsemido14@gmail.com>
2025-11-19 07:59:03 +0200

    Implement initial Generic Repository Pattern

---
+++ DomainLayer/ECommerce.Domain/Contracts/Repos/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Contracts.Repos
+{
+    // Generic Repository Interface
+    public interface IGenericRepository<TEntity, TKey> where TEntity: BaseEntity<TKey>
+    {
+        // Get all entities
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        // Get entity by id
+        Task<TEntity?> GetByIdAsync(TKey id);
+
+        // Add new entity
+        void Add(TEntity entity);
+
+        // Update existing entity
+        void Update(TEntity entity);
+
+        // Delete entity
+        void Delete(TEntity entity);
+
+    }
+}
---
+++ InfrastructureLayer/ECommerce.Persistence/Repos/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Domain.Contracts.Repos;
+using ECommerce.Domain.Models;
+using ECommerce.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Repos
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _context;
+        public GenericRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+            => await _context.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id) => await _context.Set<TEntity>().FindAsync(id);
+
+        public void Add(TEntity entity) => _context.Set<TEntity>().Add(entity);
+
+        public void Delete(TEntity entity) => _context.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity) => _context.Set<TEntity>().Update(entity);
+    }
+}
