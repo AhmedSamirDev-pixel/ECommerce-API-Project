9482b4dede1d20f90f8dd2c6d1396eced6e7b9e1
Ahmed Samir <ahmedsemido14@gmail.com>
2025-11-20 18:21:08 +0200

    Implement Service Manager pattern

---
+++ DomainLayer/ECommerce.Services/BusinessServices/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ECommerce.Domain.Contracts.UnitOfWork;
+using ECommerce.ServicesAbstraction.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services.BusinessServices
+{
+    public class ServiceManager(IMapper _mapper, IUnitOfWork _unitofwork) : IServiceManager
+    {
+        public readonly Lazy<IProductServices> lazyProductServices = 
+           new Lazy<IProductServices>(() => new ProductServices(mapper: _mapper, unitOfWork: _unitofwork));
+        public IProductServices ProductServices => lazyProductServices.Value;
+    }
+}
