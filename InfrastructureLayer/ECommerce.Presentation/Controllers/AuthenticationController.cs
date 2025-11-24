f1250f95d9b14a576c0089e427dfd8f17614037d
Ahmed Samir <ahmedsemido14@gmail.com>
2025-11-24 21:05:02 +0200

    Implement authenticated user management endpoints (CheckEmail, CurrentUser, GetAddress, UpdateAddress)

--- InfrastructureLayer/ECommerce.Presentation/Controllers/AuthenticationController.cs
+++ InfrastructureLayer/ECommerce.Presentation/Controllers/AuthenticationController.cs
@@ -1,10 +1,12 @@
 ﻿using ECommerce.ServicesAbstraction.IServices;
 using ECommerce.Shared.DTOs.IdentityDTOS;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,5 +36,42 @@ namespace ECommerce.Presentation.Controllers
             var user = await _serviceManager.AuthenticationServices.RegisterAsync(registerDTO);
             return Ok(user);
         }
+
+        [Authorize]
+        [HttpGet("CheckEmail")]
+        public async Task<ActionResult<bool>> CheckEmail(string email)
+        {
+            var result = await _serviceManager.AuthenticationServices.CheckEmailAsync(email);  
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("CurrentUser")]
+        public async Task<ActionResult<UserDTO>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var user =  await _serviceManager.AuthenticationServices.GetCurrentUserAsync(email);
+            return Ok(user);
+        }
+
+        [Authorize]
+        [HttpGet("CurrentUserAddress")]
+        public async Task<ActionResult<AddressDTO>> GetCurrentUserAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var address = await _serviceManager.AuthenticationServices.GetCurrentUserAddressAsync(email);
+            return Ok(address);
+        }
+
+        [Authorize]
+        [HttpPut("UpdateAddress")]
+        public async Task<ActionResult<AddressDTO>> UpdateCurrentUserAddress(AddressDTO addressDTO)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var updatedAddress = await _serviceManager.AuthenticationServices.UpdateCurrentUserAddressAsync(email, addressDTO);
+
+            return Ok(updatedAddress);
+        }
+
     }
 }
