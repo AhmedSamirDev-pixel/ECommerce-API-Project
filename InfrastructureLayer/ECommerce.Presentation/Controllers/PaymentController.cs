bff6c5fe75ebbed7fc1241ff95b856a6a1fa600c
Ahmed Samir <ahmedsemido14@gmail.com>
2025-11-27 20:57:58 +0200

    Add PaymentController with payment intent endpoint

---
+++ InfrastructureLayer/ECommerce.Presentation/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.ServicesAbstraction.IServices;
+using ECommerce.Shared.DTOs.BasketDTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase 
+    {
+        private readonly IServiceManager _serviceManager;
+        public PaymentController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+
+        [HttpPost("{basketId}")]
+        [Authorize]
+        public async Task<ActionResult<BasketDTO>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            var basket = await _serviceManager.PaymentServices.CreateOrUpdatePaymentIntentAsync(basketId);
+            return Ok(basket);
+        }
+    }
+}
